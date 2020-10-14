33f0f31e6b0ee46ff360e1f58418ae1dcbbd047e
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-10-14 13:06:10 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsArray = "";
+  let sameBracketsArray = "";
+  let notParCount = {};
+
+  for(let i=0; i<bracketsConfig.length; i++){
+    bracketsArray += bracketsConfig[i][0];
+    bracketsArray += bracketsConfig[i][1];
+
+    if(bracketsConfig[i][0] === bracketsConfig[i][1])
+    sameBracketsArray += bracketsConfig[i][0];
+  }
+  console.log(bracketsArray);
+
+  let stack = [];
+  
+  for(let bracket of str) {
+    let apecialIndex = sameBracketsArray.indexOf(bracket);
+    if(apecialIndex !== -1) notParCount[bracket] = notParCount[bracket] != undefined ? notParCount[bracket] + 1 : 1;
+
+      let bracketsIndex = bracketsArray.indexOf(bracket);
+      if (bracketsIndex === -1) continue;
+      if(apecialIndex !== -1 && notParCount[bracket] % 2 === 0)bracketsIndex +=1;
+
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1);
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+  }
+
+  return stack.length === 0;
 }
